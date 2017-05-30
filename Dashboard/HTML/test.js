164f3bec276be48e5347d3d58577515a8b3ad4ab
mignet-github <mignet_github@yahoo.com>
2017-05-30 11:37:35 +0530

    Some progress

---
+++ Dashboard/HTML/test.js
@@ -0,0 +1,17 @@
+/**
+ * http://usejsdoc.org/
+ */
+
+
+//function to handle multiple styles
+
+function myFunction1() {
+	   // document.getElementById("South-Solace").class = "bg-success";
+		 document.getElementById("South-Solace").className = "laurent-normal";
+	}
+
+
+function myFunction() {
+   // document.getElementById("South-Solace").class = "bg-success";
+	 document.getElementById("South-Solace").className = "laurent-danger";
+}
